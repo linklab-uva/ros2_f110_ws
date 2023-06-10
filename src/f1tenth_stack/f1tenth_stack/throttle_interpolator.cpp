2490d0fbd0a3a6f218cf0234d24eefe19a0fedd0
John Link <jwl9vq@virginia.edu>
2023-06-10 14:15:07 -0400

    converted throttle interpolator to cpp

---
+++ src/f1tenth_stack/f1tenth_stack/throttle_interpolator.cpp
@@ -0,0 +1,137 @@
+#include <rclcpp/rclcpp.hpp>
+#include <std_msgs/msg/float64.hpp>
+
+class ThrottleInterpolator : public rclcpp::Node
+{
+public:
+  ThrottleInterpolator() : Node("throttle_interpolator")
+  {
+    this->declare_parameter("rpm_input_topic", "commands/motor/unsmoothed_speed");
+    this->declare_parameter("rpm_output_topic", "commands/motor/speed");
+    this->declare_parameter("servo_input_topic", "commands/servo/unsmoothed_position");
+    this->declare_parameter("servo_output_topic", "commands/servo/position");
+    this->declare_parameter("max_acceleration", 0.0);
+    this->declare_parameter("speed_max", 0.0);
+    this->declare_parameter("speed_min", 0.0);
+    this->declare_parameter("throttle_smoother_rate", 0.0);
+    this->declare_parameter("speed_to_erpm_gain", 0.0);
+    this->declare_parameter("max_servo_speed", 0.0);
+    this->declare_parameter("steering_angle_to_servo_gain", 0.0);
+    this->declare_parameter("servo_smoother_rate", 0.0);
+    this->declare_parameter("servo_max", 0.0);
+    this->declare_parameter("servo_min", 0.0);
+    this->declare_parameter("steering_angle_to_servo_offset", 0.0);
+
+    this->get_parameter("rpm_input_topic", rpm_input_topic);
+    this->get_parameter("rpm_output_topic", rpm_output_topic);
+    this->get_parameter("servo_input_topic", servo_input_topic);
+    this->get_parameter("servo_output_topic", servo_output_topic);
+    this->get_parameter("max_acceleration", max_acceleration);
+    this->get_parameter("speed_max", max_rpm);
+    this->get_parameter("speed_min", min_rpm);
+    this->get_parameter("throttle_smoother_rate", throttle_smoother_rate);
+    this->get_parameter("speed_to_erpm_gain", speed_to_erpm_gain);
+    this->get_parameter("max_servo_speed", max_servo_speed);
+    this->get_parameter("steering_angle_to_servo_gain", steering_angle_to_servo_gain);
+    this->get_parameter("servo_smoother_rate", servo_smoother_rate);
+    this->get_parameter("servo_max", max_servo);
+    this->get_parameter("servo_min", min_servo);
+    this->get_parameter("steering_angle_to_servo_offset", last_servo);
+
+    last_rpm = 0;
+    desired_rpm = last_rpm;
+    desired_servo_position = last_servo;
+
+    rpm_output = this->create_publisher<std_msgs::msg::Float64>(rpm_output_topic, 1);
+    servo_output = this->create_publisher<std_msgs::msg::Float64>(servo_output_topic, 1);
+
+    rpm_sub = this->create_subscription<std_msgs::msg::Float64>(rpm_input_topic, 1, std::bind(&ThrottleInterpolator::_process_throttle_command, this, std::placeholders::_1));
+    servo_sub = this->create_subscription<std_msgs::msg::Float64>(servo_input_topic, 1, std::bind(&ThrottleInterpolator::_process_servo_command, this, std::placeholders::_1));
+
+    max_delta_servo = abs(steering_angle_to_servo_gain * max_servo_speed / servo_smoother_rate);
+    servo_timer = this->create_wall_timer(std::chrono::milliseconds(1.0 / servo_smoother_rate), std::bind(&ThrottleInterpolator::_publish_servo_command, this));
+
+    max_delta_rpm = abs(speed_to_erpm_gain * max_acceleration / throttle_smoother_rate);
+    rmp_timer = this->create_wall_timer(std::chrono::milliseconds(1.0 / throttle_smoother_rate), std::bind(&ThrottleInterpolator::_publish_throttle_command, this));
+  }
+
+private:
+  void _publish_throttle_command()
+  {
+    double desired_delta = desired_rpm - last_rpm;
+    double clipped_delta = std::max(std::min(desired_delta, max_delta_rpm), -max_delta_rpm);
+    double smoothed_rpm = last_rpm + clipped_delta;
+    last_rpm = smoothed_rpm;
+    // auto rpm_msg = std_msgs::msg::Float64();
+    std_msgs::msg::Float64 rpm_msg;
+    rpm_msg.data = static_cast<float>(smoothed_rpm);
+    rpm_output->publish(rpm_msg);
+  }
+
+  void _process_throttle_command(const std_msgs::msg::Float64::SharedPtr msg)
+  {
+    double input_rpm = msg->data;
+    // Do some sanity clipping
+    input_rpm = std::min(std::max(input_rpm, min_rpm), max_rpm);
+    desired_rpm = input_rpm;
+  }
+
+  void _publish_servo_command()
+  {
+    double desired_delta = desired_servo_position - last_servo;
+    double clipped_delta = std::max(std::min(desired_delta, max_delta_servo), -max_delta_servo);
+    double smoothed_servo = last_servo + clipped_delta;
+    last_servo = smoothed_servo;
+    // auto servo_msg = std::make_shared<std_msgs::msg::Float64>();
+    std_msgs::msg::Float64 servo_msg;
+    servo_msg.data = static_cast<float>(smoothed_servo);
+    servo_output->publish(servo_msg);
+  }
+
+  void _process_servo_command(const std_msgs::msg::Float64::SharedPtr msg)
+  {
+    double input_servo = msg->data;
+    // Do some sanity clipping
+    input_servo = std::min(std::max(input_servo, min_servo), max_servo);
+    // set the target servo position
+    desired_servo_position = input_servo;
+  }
+
+  rclcpp::Publisher<std_msgs::msg::Float64>::SharedPtr rpm_output;
+  rclcpp::Publisher<std_msgs::msg::Float64>::SharedPtr servo_output;
+  rclcpp::Subscription<std_msgs::msg::Float64>::SharedPtr rpm_sub;
+  rclcpp::Subscription<std_msgs::msg::Float64>::SharedPtr servo_sub;
+  rclcpp::TimerBase::SharedPtr servo_timer;
+  rclcpp::TimerBase::SharedPtr rmp_timer;
+
+  std::string rpm_input_topic;
+  std::string rpm_output_topic;
+  std::string servo_input_topic;
+  std::string servo_output_topic;
+  double max_acceleration;
+  double max_rpm;
+  double min_rpm;
+  double throttle_smoother_rate;
+  double speed_to_erpm_gain;
+  double max_servo_speed;
+  double steering_angle_to_servo_gain;
+  double servo_smoother_rate;
+  double max_servo;
+  double min_servo;
+  double last_servo;
+
+  double last_rpm;
+  double desired_rpm;
+  double desired_servo_position;
+  double max_delta_servo;
+  double max_delta_rpm;
+};
+
+int main(int argc, char **argv)
+{
+  rclcpp::init(argc, argv);
+  auto p = std::make_shared<ThrottleInterpolator>();
+  rclcpp::spin(p);
+  rclcpp::shutdown();
+  return 0;
+}
---
+++ src/f1tenth_stack/launch/move_base.launch.py
@@ -0,0 +1,138 @@
+# MIT License
+
+# Copyright (c) 2020 Hongrui Zheng
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.substitutions import Command
+from launch.substitutions import LaunchConfiguration
+from launch.actions import DeclareLaunchArgument
+from launch.actions import IncludeLaunchDescription
+from launch_xml.launch_description_sources import XMLLaunchDescriptionSource
+from ament_index_python.packages import get_package_share_directory
+import os
+
+def generate_launch_description():
+
+    car_namespace = "car_x"
+    use_sim_time = False
+
+    vesc_config = os.path.join(
+        get_package_share_directory('f1tenth_stack'),
+        'config',
+        'vesc.yaml'
+    )
+    urdf_file = os.path.join(
+        get_package_share_directory('f1tenth_stack'),
+        'urdf',
+        'f110.urdf'
+    )
+    with open(urdf_file, 'r') as infp:
+        robot_desc = infp.read()
+    sensors_config = os.path.join(
+        get_package_share_directory('f1tenth_stack'),
+        'config',
+        'sensors.yaml'
+    )
+    mux_config = os.path.join(
+        get_package_share_directory('f1tenth_stack'),
+        'config',
+        'mux.yaml'
+    )
+
+    vesc_la = DeclareLaunchArgument(
+        'vesc_config',
+        default_value=vesc_config,
+        description='Descriptions for vesc configs')
+    sensors_la = DeclareLaunchArgument(
+        'sensors_config',
+        default_value=sensors_config,
+        description='Descriptions for sensor configs')
+    mux_la = DeclareLaunchArgument(
+        'mux_config',
+        default_value=mux_config,
+        description='Descriptions for ackermann mux configs')
+
+    ld = LaunchDescription([vesc_la, sensors_la, mux_la])
+
+    ackermann_to_vesc_node = Node(
+        package='vesc_ackermann',
+        executable='ackermann_to_vesc_node',
+        namespace=car_namespace,
+        name='ackermann_to_vesc_node',
+        parameters=[LaunchConfiguration('vesc_config')]
+    )
+    vesc_to_odom_node = Node(
+        package='vesc_ackermann',
+        executable='vesc_to_odom_node',
+        namespace=car_namespace,
+        name='vesc_to_odom_node',
+        parameters=[LaunchConfiguration('vesc_config')]
+    )
+    vesc_driver_node = Node(
+        package='vesc_driver',
+        executable='vesc_driver_node',
+        namespace=car_namespace,
+        name='vesc_driver_node',
+        parameters=[LaunchConfiguration('vesc_config')]
+    )
+    throttle_interpolator_node = Node(
+        package='f1tenth_stack',
+        executable='throttle_interpolator',
+        namespace=car_namespace,
+        name='throttle_interpolator',
+        parameters=[LaunchConfiguration('vesc_config')]
+    )
+    urg_node = Node(
+        package='urg_node',
+        executable='urg_node_driver',
+        namespace=car_namespace,
+        name='urg_node',
+        parameters=[LaunchConfiguration('sensors_config')]
+    )
+    ackermann_mux_node = Node(
+        package='ackermann_mux',
+        executable='ackermann_mux',
+        namespace=car_namespace,
+        name='ackermann_mux',
+        parameters=[LaunchConfiguration('mux_config')],
+        remappings=[('ackermann_cmd_out', 'ackermann_drive')]
+    )
+
+    robot_state_publisher = Node(
+            package='robot_state_publisher',
+            executable='robot_state_publisher',
+            namespace=car_namespace,
+            name='robot_state_publisher',
+            output='screen',
+            parameters=[{'use_sim_time': use_sim_time, 'robot_description': robot_desc}],
+            arguments=[urdf_file]
+    )
+
+    ld.add_action(ackermann_to_vesc_node)
+    ld.add_action(vesc_to_odom_node)
+    ld.add_action(vesc_driver_node)
+    ld.add_action(throttle_interpolator_node)
+    ld.add_action(urg_node)
+    ld.add_action(ackermann_mux_node)
+    ld.add_action(robot_state_publisher)
+
+    return ld
