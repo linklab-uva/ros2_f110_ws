7e1c9b30bead36cf00859a66b2fae58fef6f8cf1
pc1 <pc1>
2023-06-15 15:10:07 -0400

    updated deps script

--- scripts/install_deps.sh
+++ scripts/install_deps.sh
@@ -1,2 +1,2 @@
 
-apt-get install ros-$ROS_DISTRO-urg-node ros-$ROS_DISTRO-joy-teleop ros-$ROS_DISTRO-serial-driver
+sudo apt-get install ros-$ROS_DISTRO-urg-node ros-$ROS_DISTRO-joy-teleop ros-$ROS_DISTRO-serial-driver ros-$ROS_DISTRO-diagnostic-updater 
